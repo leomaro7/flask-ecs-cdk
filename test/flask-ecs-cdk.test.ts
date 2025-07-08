8a3c8f80209aba066faec8fe936f35b6fc607d7a
leomarokun <xxx@gmail.com>
2025-07-08 14:34:06 +0900

    Initial commit

---
+++ test/flask-ecs-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as FlaskEcsCdk from '../lib/flask-ecs-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/flask-ecs-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FlaskEcsCdk.FlaskEcsCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
